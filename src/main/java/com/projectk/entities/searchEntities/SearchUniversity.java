1f105b64f55a483a70c588b15c0a747924fc68a6
basaso <biako17@freeuni.edu.ge>
2020-07-17 18:11:08 +0400

    modified UniversityManager,sql search scripts

--- src/main/java/com/projectk/entities/searchEntities/SearchUniversity.java
+++ src/main/java/com/projectk/entities/searchEntities/SearchUniversity.java
@@ -1,4 +1,80 @@
 package com.projectk.entities.searchEntities;
 
+import com.projectk.entities.Faculty;
+import com.projectk.entities.FacultyCategory;
+
 public class SearchUniversity {
+
+    private Faculty faculty;
+
+    private String universityName;
+
+    private Integer universityId;
+
+    private String city;
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public Integer getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(Integer universityId) {
+        this.universityId = universityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public static class Builder{
+        private Faculty faculty;
+
+        private String universityName;
+
+        private Integer universityId;
+
+        private String city;
+
+        public SearchUniversity.Builder universityId(Integer val){
+            universityId=val;
+            return this;
+        }
+        public SearchUniversity.Builder faculty(Faculty fac){
+            faculty=fac;
+            return this;
+        }
+        public SearchUniversity.Builder city(String cityVal){
+            city=cityVal;
+            return this;
+        }
+        public SearchUniversity.Builder universityName(String UniName){
+            universityName = UniName;
+            return this;
+        }
+    }
+
+    private SearchUniversity(SearchUniversity.Builder builder){
+        this.faculty = builder.faculty;
+        this.city = builder.city;
+        this.universityId = builder.universityId;
+        this.universityName = builder.universityName;
+    }
 }
--- src/main/java/com/projectk/storage/implementations/UniversityManager.java
+++ src/main/java/com/projectk/storage/implementations/UniversityManager.java
@@ -1,29 +1,62 @@
 package com.projectk.storage.implementations;
 
+import com.projectk.entities.Faculty;
 import com.projectk.entities.University;
+import com.projectk.entities.searchEntities.SearchFaculty;
 import com.projectk.entities.searchEntities.SearchUniversity;
+import com.projectk.storage.connectionFactory.DatabaseConnection;
 import com.projectk.storage.interfaces.helpers.BaseStorage;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class UniversityManager implements BaseStorage <University, SearchUniversity> {
-	@Override
-	public List<University> filter(SearchUniversity searchEntity) {
-		return null;
-	}
+public class UniversityManager implements BaseStorage<University, SearchUniversity> {
+    private DatabaseConnection databaseConnection;
 
-	@Override
-	public void add(University entity) {
+    public UniversityManager(DatabaseConnection databaseConnection) {
+        this.databaseConnection = databaseConnection;
+    }
 
-	}
+    @Override
+    public List<University> filter(SearchUniversity searchEntity) throws SQLException {
+        Connection connection = databaseConnection.getConnection();
+        String sqlCommand = getSqlCommand(searchEntity);
+        Statement statement = connection.prepareStatement(sqlCommand);
 
-	@Override
-	public void delete(University entity) {
+        return null;
+    }
 
-	}
+    private String getSqlCommand(SearchUniversity searchUniversity) {
+        StringBuilder builder = new StringBuilder("SELECT * FROM university U WHERE 1 = 1");
+        if (searchUniversity.getUniversityId() != null) {
+            builder.append("and U.id = ?");
+        }
+		if (searchUniversity.getCity() != null) {
+			builder.append("and U.city = ?");
+		}
+		if (searchUniversity.getUniversityName() != null) {
+			builder.append("and U.university_name = ?");
+		}
+		if (searchUniversity.getFaculty() != null) {
+			builder.append("and U.faculty = ?");
+		}
+        return null;
+    }
 
-	@Override
-	public void update(University entity) {
+    @Override
+    public void add(University entity) {
 
-	}
+    }
+
+    @Override
+    public void delete(University entity) {
+
+    }
+
+    @Override
+    public void update(University entity) {
+
+    }
 }
