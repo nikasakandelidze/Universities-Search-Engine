89f4f082bccf9247fcde5fedd242152ff13c1f59
khivanova <livan15@freeuni.edu.ge>
2020-07-19 18:06:46 +0400

    subjectManager class , update , delete  and add methods implemented
    (cherry picked from commit 835260f0ca18cbbbfc3007964dfb49a4d90d6f2d)

---
+++ src/main/java/com/projectk/storage/storageManager/implementations/subjectManager/executableStatementGenerator/JdbcExecutableStatementGenerator.java
@@ -0,0 +1,42 @@
+package com.projectk.storage.storageManager.implementations.subjectManager.executableStatementGenerator;
+
+import com.projectk.storage.connectionManager.customExceptions.StorageException;
+import com.projectk.storage.storageManager.implementations.subjectManager.executableStatementGenerator.helpers.PreparedStatementGenerator.interfaces.PreparedStatementGenerator;
+import com.projectk.storage.storageManager.implementations.subjectManager.executableStatementGenerator.helpers.sqlQueryGenerators.interfaces.WildCardSqlQueryGenerator;
+
+import java.sql.SQLException;
+
+public class JdbcExecutableStatementGenerator<T,Q> implements ExecutableStatementGenerator<T,Q> {
+    private PreparedStatementGenerator<T> preparedStatementGenerator;
+    private WildCardSqlQueryGenerator<T> wildCardSqlQueryGenerator;
+
+    public JdbcExecutableStatementGenerator(PreparedStatementGenerator<T> preparedStatementGenerator,
+                                            WildCardSqlQueryGenerator<T> wildCardSqlQueryGenerator){
+        this.preparedStatementGenerator = preparedStatementGenerator;
+        this.wildCardSqlQueryGenerator = wildCardSqlQueryGenerator;
+    }
+
+    @Override
+    public Object getSelectExecutableStatement(T searchEntity) throws StorageException {
+        try {
+            return preparedStatementGenerator.generatePreparedStatementFromSqlQuery(wildCardSqlQueryGenerator.generateWildCardQueryFromSearchEntity(searchEntity),searchEntity);
+        } catch (SQLException throwables) {
+            throw new StorageException(throwables);
+        }
+    }
+
+    @Override
+    public Object getAddExecutableStatement(Q newEntity) {
+        return null;
+    }
+
+    @Override
+    public Object getUpdateExecutableStatement(Q newEntity) {
+        return null;
+    }
+
+    @Override
+    public Object getDeleteExecutableStatement(int id) {
+        return null;
+    }
+}
