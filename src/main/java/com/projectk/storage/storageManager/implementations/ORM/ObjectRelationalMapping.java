629cadfb5c37baf16680c62dfd51db041e1b89d0
sakana <nikasakana@gmail.com>
2020-07-19 15:32:53 +0400

    t

--- src/main/java/com/projectk/storage/storageManager/implementations/ORM/ObjectRelationalMapping.java
+++ src/main/java/com/projectk/storage/storageManager/implementations/ORM/ObjectRelationalMapping.java
@@ -2,9 +2,9 @@ package com.projectk.storage.storageManager.implementations.ORM;
 
 import java.util.List;
 
-public interface ObjectRelationalMapping<T> {
-    List<?> filter(T searchEntity);
-    T add(T entity);
+public interface ObjectRelationalMapping<T,Q> {
+    List<T> filter(Q searchEntity);
+    T add(Q entity);
     T delete(T entity);
     T update(T entity);
 }
