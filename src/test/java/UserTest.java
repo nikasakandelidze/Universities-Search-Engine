c78e39becdd7bb1fffefaa24010d5b0ebe96d5b4
basaso <biako17@freeuni.edu.ge>
2020-07-26 18:41:57 +0400

    User database checking by University Id

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -21,7 +21,7 @@ public class UserTest {
     private UserManager manager;
 
     @Test
-    public void testSubjectManagerByName() throws SQLException, StorageException {
+    public void testUserByUsername() throws SQLException, StorageException {
         ConnectionManager connectionManager = new MysqlConnectionManager();
         manager = new UserManager(connectionManager);
         String name = "test";
@@ -47,5 +47,53 @@ public class UserTest {
         Assert.notEmpty(subjectList, "jkefkef");
         assertTrue(subjectList.contains(user));
 
+    }
+
+    @Test
+    public void testUserUpateAndDelete() throws StorageException, SQLException {
+        ConnectionManager connectionManager = new MysqlConnectionManager();
+        manager = new UserManager(connectionManager);
+        String name = "test";
+        User user = new User.Builder()
+                .university_id(1)
+                .username("test1")
+                .encoded_password("test1")
+                .build();
+        User updatedUser = new User.Builder()
+                .university_id(1)
+                .username("test1")
+                .encoded_password("test" + "test")
+                .build();
+        SearchUser searchUser = new SearchUser.Builder()
+                .userName(user.getUsername())
+                .build();
+        List<User> subjectList = new ArrayList<>();
+        try {
+            manager.delete(user);
+            manager.add(user);
+            subjectList = (manager.filter(user));
+        } catch (StorageException | SQLException e) {
+            e.printStackTrace();
+        }
+        Optional<User> found = subjectList
+                .stream()
+                .findAny();
+        Assert.notEmpty(subjectList, "jkefkef");
+        assertTrue(subjectList.contains(user));
+        manager.update(updatedUser);
+        SearchUser searchUser1 = new SearchUser.Builder()
+                .userName(updatedUser.getUsername())
+                .build();
+
+        subjectList = (manager.filter(updatedUser));
+        Assert.notEmpty(subjectList, "jkefkef");
+        assertTrue(!subjectList.contains(user));
+        assertTrue(subjectList.contains(updatedUser));
+        manager.delete(user);
+        manager.delete(updatedUser);
+        subjectList = (manager.filter(updatedUser));
+        assertTrue(!subjectList.contains(updatedUser));
+        subjectList = (manager.filter(user));
+        assertTrue(!subjectList.contains(user));
     }
 }
