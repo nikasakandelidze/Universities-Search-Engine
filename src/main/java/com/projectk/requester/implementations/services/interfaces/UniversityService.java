b8b1bae19aaf2203135e68ce6523a60c3decadea
acho01 <archil.sharashenidze@azry.com>
2020-07-29 20:12:52 +0400

    serviceResult import fix

--- src/main/java/com/projectk/requester/implementations/services/interfaces/UniversityService.java
+++ src/main/java/com/projectk/requester/implementations/services/interfaces/UniversityService.java
@@ -1,9 +1,9 @@
 package com.projectk.requester.implementations.services.interfaces;
 
+import com.projectk.entities.ServiceResult;
 import com.projectk.entities.University;
 import com.projectk.entities.User;
 import com.projectk.entities.searchEntities.SearchUniversity;
-import com.projectk.requester.implementations.services.ServiceResult;
 
 public interface UniversityService {
 	ServiceResult addUniversity(University university);
