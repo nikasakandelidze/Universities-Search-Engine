4d8c7906b0f849470ca8076c54b8b838291ba77d
khivanova <livan15@freeuni.edu.ge>
2020-08-01 02:58:09 +0400

    map fix, back to hashmap from concurrent hashmap

--- src/main/java/com/projectk/services/implementations/utils/HomePageUtils.java
+++ src/main/java/com/projectk/services/implementations/utils/HomePageUtils.java
@@ -1,5 +1,6 @@
 package com.projectk.services.implementations.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -8,7 +9,7 @@ import java.util.function.Predicate;
 public class HomePageUtils {
 	public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor)
 	{
-		Map<Object, Boolean> map = new ConcurrentHashMap<>();
+		Map<Object, Boolean> map = new HashMap<>();
 		return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
 	}
 }
--- src/main/java/com/projectk/storage/connectionManager/StorageCustomerInfo.java
+++ src/main/java/com/projectk/storage/connectionManager/StorageCustomerInfo.java
@@ -3,5 +3,5 @@ package com.projectk.storage.connectionManager;
 public class StorageCustomerInfo {
     public static final String DB_NAME = "projectK";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "admin";
+    public static final String DB_PASSWORD = "Karameli123#";
 }
